b1d431b81af0b00e41a7c152b6e89da124540153
Seanmclem <seanmclements@gmail.com>
2021-06-22 20:50:03 -0400

    basic users api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import express, { Router } from "express";
 import cors from "cors";
 import fs from "fs";
 
+export interface User {
+  address: string;
+  clientid: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  role: "client" | "admin" | "user";
+}
+
 const app = express();
 
 // const allowedOrigins = ["http://localhost:3000"];
@@ -17,12 +27,39 @@ app.listen(3100, () => {
 
 const router = Router();
 
-app.get("/users", async (req, res, next) => {
-  let rawdata = fs.readFileSync("server/users.json");
-  let users = JSON.parse(rawdata as any);
+app.get("/login", async (req, res, next) => {
+  // const { email } = req.body;
+  const { email } = req.query as any;
 
-  res.json(users);
+  let rawdata = fs.readFileSync("server/users.json");
+  let data: { users: User[] } = JSON.parse(rawdata as any);
+  const user = data?.users.find((u) => u.email === email);
+  // console.log(user);
+  res.send(user ? { user: user } : {});
 });
+
+app.post("/users", async (req, res, next) => {
+  const { email } = req.body;
+  console.log(req.body);
+  let rawdata = fs.readFileSync("server/users.json");
+  let data: { users: User[] } = JSON.parse(rawdata as any);
+  const user = data?.users.find((u) => u.email === email);
+  const users = getUsersForRole(data?.users, user);
+  res.json({ users });
+});
+
+const getUsersForRole = (users: User[], user?: User) => {
+  switch (user?.role) {
+    case "admin":
+      return users;
+    case "client":
+      return users.filter((u) => user.clientid === u.clientid);
+    case "user":
+      return [user];
+    default:
+      return [];
+  }
+};
 
 // router.post('/user', checkIfAuthenticated, async (req, res) => {
 //   return UserController.insert(req, res);
